ed4b8f2b4e66030963c388fc0195b00fee4f8f79
Benoît Béranger <benoit.beranger@icm-institute.org>
2017-05-03 12:54:24 +0200

    all contrasts are coded and automatic

--- firstlevel_prisma_execution.m
+++ firstlevel_prisma_execution.m
@@ -21,9 +21,11 @@ par.run=1;
 
 dfonc = get_subdir_regex_multi(suj,par.dfonc_reg); char(dfonc{:})
 ffonc = get_subdir_regex_files(dfonc,'^swutrf')
-
+nrRun = size(ffonc{1},1);
 
 %% my onsets
+
+time_offcet = +0.400; % seconds // due to the format_vmrk.m, the t0 read on Analyser is shifted
 
 % Run 1
 
@@ -37,19 +39,19 @@ LRLR_1 = [
     177.816 182.964
     198.104 203.512
     215.320 221.116
-    ];
+    ]+time_offcet;
 
 countdown_1 = [
     10.508 22.432
     76.588 94.920
     182.964 198.104
-    ];
+    ]+time_offcet;
 
 sequence_1 = [
     27.204 38.324
     100.912 113.436
     203.512 215.320
-    ];
+    ]+time_offcet;
 
 % Run 2
 
@@ -60,19 +62,19 @@ LRLR_2 = [
     79.680 84.872
     100.732 107.680
     120.032 126.464
-    ];
+    ]+time_offcet;
 
 countdown_2 = [
     17.872 30.732
     84.82 100.732
     171.656 187.372
-    ];
+    ]+time_offcet;
 
 sequence_2 = [
     36.468 47.860
     107.680 120.032
     194.236 207.284
-    ];
+    ]+time_offcet;
 
 % Run 3
 
@@ -86,19 +88,19 @@ LRLR_3 = [
     177.772 183.280
     200.380 206.724
     219.188 225.276
-    ];
+    ]+time_offcet;
 
 countdown_3 = [
     24.380 37.328
     103.120 116.556
     183.280 200.380
-    ];
+    ]+time_offcet;
 
 sequence_3 = [
     43.640 56.032
     122.932 134.428
     183.280 219.188
-    ];
+    ]+time_offcet;
 
 %% Onsets formating
 
@@ -145,7 +147,7 @@ fspm = get_subdir_regex_files(execdir,'SPM',1)
 j_estimate = job_first_level12_estimate(fspm,par)
 
 
-%% Define contrast
+%% Define contrast: all runs
 
 contrast.names = {
     
@@ -176,7 +178,89 @@ contrast.types = cat(1,repmat({'T'},[1 length(contrast.names)]));
 par.delete_previous=1;
 
 
-%% Estimate contrast
+%% Estimate contrast : all runs
+
+par.run = 1;
+par.display = 0;
+
+j_contrast_rep = job_first_level12_contrast_repl(fspm,contrast,par)
+par.delete_previous=0;
+
+
+%% Define contrast: each run
+
+contrast.names = {};
+for run = 1 : nrRun
+contrast.names = [contrast.names sprintf('%d - main effect : LRLR'     ,run)];
+contrast.names = [contrast.names sprintf('%d - main effect : countdown',run)];
+contrast.names = [contrast.names sprintf('%d - main effect : sequence' ,run)];
+contrast.names = [contrast.names sprintf('%d - LRLR - countdown'       ,run)];
+contrast.names = [contrast.names sprintf('%d - LRLR - sequence'        ,run)];
+contrast.names = [contrast.names sprintf('%d - countdown - LRLR'       ,run)];
+contrast.names = [contrast.names sprintf('%d - countdown - sequence'   ,run)];
+contrast.names = [contrast.names sprintf('%d - sequence - LRLR'        ,run)];
+contrast.names = [contrast.names sprintf('%d - sequence - countdown'   ,run)];
+end
+
+
+
+ main_contrasts = {
+    [ 1 0 0 ]
+    [ 0 1 0 ]
+    [ 0 0 1 ]
+    [ 1 -1 0 ]
+    [ 1 0 -1 ]
+    [ -1 1 0 ]
+    [ 0 1 -1 ]
+    [ -1 0 1 ]
+    [ 0 -1 1 ]
+    };
+
+run1_full_contrasts = {};
+for c = 1 : length(main_contrasts)
+    run1_full_contrasts{c} = [ main_contrasts{c} [ 0 0 0 0 0 0 ] repmat([ main_contrasts{c}*0 [ 0 0 0 0 0 0 ] ], [1 nrRun-1]) ];
+end
+
+contrast.values = {};
+for run = 1 : nrRun
+    
+    for c = 1 : length(main_contrasts)
+        contrast.values = [ contrast.values circshift( run1_full_contrasts{c} , [0 9*(run-1)] ) ];
+    end
+
+end
+contrast.values = contrast.values;
+
+contrast.types = cat(1,repmat({'T'},[1 length(contrast.names)]));
+
+
+%% Estimate contrast : each run
+
+par.run = 1;
+par.display = 0;
+
+j_contrast_rep = job_first_level12_contrast(fspm,contrast,par)
+
+
+%% Define contrast: leaning
+
+contrast.names = {
+    
+'Increase of activation with learning : Hand vs Eye'
+'Decrease of activation with learning : Hand vs Eye'
+
+}';
+
+contrast.values = {
+    [ -2 0 1 [0 0 0 0 0 0] -2 0 2 [0 0 0 0 0 0] -2 0 3 [0 0 0 0 0 0] ]
+    [ -2 0 3 [0 0 0 0 0 0] -2 0 2 [0 0 0 0 0 0] -2 0 1 [0 0 0 0 0 0] ]
+    
+    };
+
+contrast.types = cat(1,repmat({'T'},[1 length(contrast.names)]));
+
+
+%% Estimate contrast : learning
 
 par.run = 1;
 par.display = 0;
@@ -185,6 +269,7 @@ j_contrast_rep = job_first_level12_contrast(fspm,contrast,par)
 
 
 %% Display
+
 
 show{1}.spm.stats.results.spmmat = fspm;
 show{1}.spm.stats.results.conspec.titlestr = '';
