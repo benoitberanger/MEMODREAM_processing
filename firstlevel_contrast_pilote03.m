aca36b9834cbbf50e071bfd5e913af50979e2b01
Benoît Béranger <benoit.beranger@icm-institute.org>
2017-10-31 18:37:13 +0100

    added some contrasts for pilote03

--- firstlevel_contrast_pilote03.m
+++ firstlevel_contrast_pilote03.m
@@ -17,6 +17,8 @@ par.sessrep = 'none';
 
 %% Define contrasts
 
+% Tap
+
 rp = [0 0 0 0 0 0];
 
 % 2 conditions for each run
@@ -28,17 +30,19 @@ tap  = [0 1 rp];
 
 contrast(1).names = {
     
-'Tap-Rest : DT_Simple  : Pre'
-'Tap-Rest : DT_Complex : Pre'
-'Tap-Rest : SpeedTest  : Pre'
-'Tap-Rest : SpeedTest  : Post'
-'Tap-Rest : DT_Simple  : Post'
-'Tap-Rest : DT_Complex : Post'
+'Session 1 : Tap-Rest : DT_Simple  : Pre'
+'Session 1 : Tap-Rest : DT_Complex : Pre'
+'Session 1 : Tap-Rest : SpeedTest  : Pre'
+'Session 1 : Tap-Rest : SpeedTest  : Post'
+'Session 1 : Tap-Rest : DT_Simple  : Post'
+'Session 1 : Tap-Rest : DT_Complex : Post'
 
 
-'Tap : Post-Pre : DT_Simple'
-'Tap : Post-Pre : DT_Complex'
-'Tap : Post-Pre : SpeedTest'
+'Session 1 : Tap : Post-Pre : DT_Simple'
+'Session 1 : Tap : Post-Pre : DT_Complex'
+'Session 1 : Tap : Post-Pre : SpeedTest'
+
+'Session 1 : Tap : Pre : DT_Complex-SpeedTest'
 
 }';
 
@@ -55,6 +59,8 @@ contrast(1).values = {
 [ null -tap null null null tap  ]
 [ null null -tap tap  null null ]
 
+[ null tap -tap null null null ]
+
 }';
 
 contrast(1).types = cat(1,repmat({'T'},[1 length(contrast(1).names)]));
@@ -64,16 +70,19 @@ contrast(1).types = cat(1,repmat({'T'},[1 length(contrast(1).names)]));
 
 contrast(2).names = {
     
-'Tap-Rest : DT_Simple  : Pre'
-'Tap-Rest : DT_Complex : Pre'
-'Tap-Rest : SpeedTest  : Pre'
-'Tap-Rest : SpeedTest  : Post'
-'Tap-Rest : DT_Simple  : Post'
-'Tap-Rest : DT_Complex : Post'
+'Session 2 : Tap-Rest : DT_Simple  : Pre'
+'Session 2 : Tap-Rest : DT_Complex : Pre'
+'Session 2 : Tap-Rest : SpeedTest  : Pre'
+'Session 2 : Tap-Rest : SpeedTest  : Post'
+'Session 2 : Tap-Rest : DT_Simple  : Post'
+'Session 2 : Tap-Rest : DT_Complex : Post'
 
-'Tap : Post-Pre : DT_Simple'
-'Tap : Post-Pre : DT_Complex'
-'Tap : Post-Pre : SpeedTest'
+'Session 2 : Tap : Post-Pre : DT_Simple'
+'Session 2 : Tap : Post-Pre : DT_Complex'
+'Session 2 : Tap : Post-Pre : SpeedTest'
+
+'Session 2 : Tap : Pre : DT_Complex-SpeedTest'
+
 
 }';
 
@@ -90,6 +99,8 @@ contrast(2).values = {
 [ null -tap null null null tap  ]
 [ null null -tap tap  null null ]
 
+[ null tap -tap null null null ]
+
 }';
 
 contrast(2).types = cat(1,repmat({'T'},[1 length(contrast(2).names)]));
@@ -99,16 +110,18 @@ contrast(2).types = cat(1,repmat({'T'},[1 length(contrast(2).names)]));
 
 contrast(3).names = {
     
-'Tap-Rest : DT_Simple  : Pre'
-'Tap-Rest : DT_Complex : Pre'
-'Tap-Rest : SpeedTest  : Pre'
-'Tap-Rest : SpeedTest  : Post'
-'Tap-Rest : DT_Simple  : Post'
-'Tap-Rest : DT_Complex : Post'
+'Session 3 : Tap-Rest : DT_Simple  : Pre'
+'Session 3 : Tap-Rest : DT_Complex : Pre'
+'Session 3 : Tap-Rest : SpeedTest  : Pre'
+'Session 3 : Tap-Rest : SpeedTest  : Post'
+'Session 3 : Tap-Rest : DT_Simple  : Post'
+'Session 3 : Tap-Rest : DT_Complex : Post'
 
-'Tap : Post-Pre : DT_Simple'
-'Tap : Post-Pre : DT_Complex'
-'Tap : Post-Pre : SpeedTest'
+'Session 3 : Tap : Post-Pre : DT_Simple'
+'Session 3 : Tap : Post-Pre : DT_Complex'
+'Session 3 : Tap : Post-Pre : SpeedTest'
+
+'Session 3 : Tap : Pre : DT_Complex-SpeedTest'
 
 }';
 
@@ -125,6 +138,8 @@ contrast(3).values = {
 [ null -tap null null null tap  ]
 [ null null -tap tap  null null ]
 
+[ null tap -tap null null null ]
+
 }';
 
 contrast(3).types = cat(1,repmat({'T'},[1 length(contrast(3).names)]));
@@ -134,24 +149,26 @@ contrast(3).types = cat(1,repmat({'T'},[1 length(contrast(3).names)]));
 
 contrast(4).names = {
     
-'Tap-Rest : DT_Simple  : PreI'
-'Tap-Rest : DT_Complex : PreI'
-'Tap-Rest : SpeedTest  : PreI'
-'Tap-Rest : SpeedTest  : PostI'
-'Tap-Rest : DT_Simple  : PostIPreE'
-'Tap-Rest : DT_Complex : PostIPreE'
-'Tap-Rest : SpeedTest  : PreI'
-'Tap-Rest : SpeedTest  : PostI'
-'Tap-Rest : DT_Simple  : PostI'
-'Tap-Rest : DT_Complex : PostI'
+'Session 4 : Tap-Rest : DT_Simple  : PreE'
+'Session 4 : Tap-Rest : DT_Complex : PreE'
+'Session 4 : Tap-Rest : SpeedTest  : PreE'
+% Execution
+'Session 4 : Tap-Rest : SpeedTest  : PostE'
+'Session 4 : Tap-Rest : DT_Simple  : PostEPreI'
+'Session 4 : Tap-Rest : DT_Complex : PostEPreI'
+'Session 4 : Tap-Rest : SpeedTest  : PreI'
+% Imagination
+'Session 4 : Tap-Rest : SpeedTest  : PostI'
+'Session 4 : Tap-Rest : DT_Simple  : PostI'
+'Session 4 : Tap-Rest : DT_Complex : PostI'
 
-'Post-Pre EXEC : DT_Simple'
-'Post-Pre EXEC : DT_Complex'
-'Post-Pre EXEC : SpeedTest'
+'Session 4 : Post-Pre EXEC : DT_Simple'
+'Session 4 : Post-Pre EXEC : DT_Complex'
+'Session 4 : Post-Pre EXEC : SpeedTest'
 
-'Post-Pre IMAG : DT_Simple'
-'Post-Pre IMAG : DT_Complex'
-'Post-Pre IMAG : SpeedTest'
+'Session 4 : Post-Pre IMAG : DT_Simple'
+'Session 4 : Post-Pre IMAG : DT_Complex'
+'Session 4 : Post-Pre IMAG : SpeedTest'
 
 }';
 
@@ -191,7 +208,7 @@ j_contrast_4 = job_first_level_contrast(fspm(4),contrast(4),par);
 
 %% Show results
 
-matlabbatch{1}.spm.stats.results.spmmat = fspm(1);
+matlabbatch{1}.spm.stats.results.spmmat = fspm(4);
 matlabbatch{1}.spm.stats.results.conspec.titlestr = '';
 matlabbatch{1}.spm.stats.results.conspec.contrasts = 1;
 matlabbatch{1}.spm.stats.results.conspec.threshdesc = 'FWE';
